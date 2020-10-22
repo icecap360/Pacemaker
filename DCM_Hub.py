c26bdd52a12e7f2b2f35a33de1a471a67c96c7fd
SamueldeHaan <72811619+SamueldeHaan@users.noreply.github.com>
2020-10-22 10:42:22 -0400

    Add files via upload

---
+++ DCM_Hub.py
@@ -0,0 +1,61 @@
+from tkinter import *
+
+#Button Functions
+def openAOO():
+    # Pacing screen screen 
+
+    return
+
+def openVOO():
+    # Pacing screen screen 
+    
+    return
+
+def openAAI():
+    # Pacing screen screen 
+    
+    return
+
+def openVVI():
+    # Pacing screen screen 
+    
+    return
+
+def openSettings():
+    # Settings screen screen 
+    
+    return
+
+def Pace():
+    #Program to pace
+
+    return
+
+#create main hub
+mainHub= Tk()
+mainHub.title("DCM HUB")
+mainHub.geometry("350x300")
+
+# create Labels
+mainHubLabel= Label(mainHub, text="Main Hub", font=(None, 50, 'underline'))
+
+# create Buttons
+AOOLink= Button(mainHub, text="AOO", command= openAOO)
+VOOLink= Button(mainHub, text="VOO", command= openVOO)
+AAILink= Button(mainHub, text="AAI", command= openAAI)
+VVILink= Button(mainHub, text="VVI", command= openVVI)
+
+SettingsLink= Button(mainHub, text="Settings")
+
+PaceNowLink= Button(mainHub, text="Pace Now", command= Pace)
+
+# organize objects 
+mainHubLabel.grid(row= 0, column= 1)
+AOOLink.grid(row=2, column=0)
+VOOLink.grid(row=3, column=0)
+AAILink.grid(row=4, column=0)
+VVILink.grid(row=5, column=0)
+
+SettingsLink.grid(row=6, column=0, pady= 20)
+
+mainloop()
---
+++ DCM_WelcomeScreen.py
@@ -0,0 +1,68 @@
+from tkinter import *
+
+userDatabase=[]
+#functions to Login and Create newUser
+def checkLogin():
+    if (user.get(),password.get()) in userDatabase:
+        print("PASS") #change to open HUB
+        return
+    else:
+        print("FAIL") #change to make fail Label
+        loginStatus= Label(welcomeScreen, text="Username or Password incorrect")
+        loginStatus.grid(row= 4, column= 2)
+        return
+
+def createNewUser():
+    for i in userDatabase:
+        if userNew.get() == i[0]:
+            print("user already made")
+            createUserStatus= Label(welcomeScreen, text="Username Taken")
+            createUserStatus.grid(row= 8, column= 2)
+            return
+    if len(userDatabase) == 10:
+        print("user data base full")
+        createUserStatus= Label(welcomeScreen, text="Maximum Users Already made")
+        createUserStatus.grid(row= 8, column= 2)
+        return
+    else:
+        userDatabase.append((userNew.get(),passwordNew.get()))
+        createUserStatus= Label(welcomeScreen, text="Use Created")
+        createUserStatus.grid(row= 8, column= 2)
+        return
+
+# create main Welcome Screen
+welcomeScreen= Tk()
+welcomeScreen.title('Pacemaker DCM')
+welcomeScreen.geometry("400x400")
+
+# create Labels
+welcomeLabel= Label(welcomeScreen, text="Welcome", font=(None, 50, 'underline'))
+loginLabel= Label(welcomeScreen, text="Login:", font=(None, 25))
+newUserLabel= Label(welcomeScreen, text="New User:", font=(None, 25))
+
+# create Entries 
+user= Entry(welcomeScreen, width= 25)
+user.insert(0, "Enter Your Username")
+password= Entry(welcomeScreen, width=25)
+password.insert(0, "Enter You Password")
+userNew= Entry(welcomeScreen, width= 25)
+userNew.insert(0, "Enter Your Username")
+passwordNew= Entry(welcomeScreen, width=25)
+passwordNew.insert(0, "Enter You Password")
+
+# create Buttons
+login= Button(welcomeScreen, text="Login", command=checkLogin)
+makeNewUser= Button(welcomeScreen, text="Make New User", command=createNewUser)
+
+# organize objects
+welcomeLabel.grid(row=0, column=0)
+loginLabel.grid(row=1, column=0)
+newUserLabel.grid(row=5, column=0)
+user.grid(row=2, column= 0)
+password.grid(row=3, column=0)
+userNew.grid(row=6, column=0)
+passwordNew.grid(row=7, column=0)
+login.grid(row=4, column=1)
+makeNewUser.grid(row=8, column=1)
+
+mainloop()
