3fac5bbff344a2cce49b37a44e9c37e8778bf573
muhammad ali <muhammadaliog3@live.com>
2020-11-20 12:28:04 -0500

    Adding uint16 params, does not work reliably, perhaps split into multiple modules

--- serial_comm_try.py
+++ serial_comm_try.py
@@ -1,6 +1,7 @@
 import serial
 import time
 from serial.tools import list_ports
+import itertools
 port = None
 baudrate = 115200
 for p in list_ports.comports():
@@ -10,13 +11,15 @@ for p in list_ports.comports():
 port_name = port.device
 with serial.Serial(port=port_name, baudrate=baudrate) as device:
 	test_code = [10]
-	params = [1,2,3,4,5,6,7,8,9,10]
-	dat = test_code+params
+	#params_uint8 = [1,2,3,4,5,6,7,8,9,10]
+	#params_uint16 = list(itertools.chain(*[[9,8],[7,6],[5,4],[3,2]]))
+	#dat = test_code+params_uint8+params_uint16
+	dat = test_code+[i for i in range(18)]
 	dat = serial.to_bytes(dat)
 	bytes_written = device.write(dat)
-
+time.sleep(0.2)
 with serial.Serial(port=port_name, baudrate=baudrate) as device:
-	bytes_read = device.read(len(params))
+	bytes_read = device.read(18)
 	#bytes_read2 = device.read(bytes_written) 
 	#we must read twice, if you read just once you get the old result, 
 	#I tried adding some delay as well, but 
@@ -25,8 +28,7 @@ print('Data to write')
 for d in dat:
 	print(d, end=' ')
 print()
-print('According to the simulink' 
-	' parameters you set, we wrote', bytes_written,'bytes')
-print('These are the bytes we read')
+print('According to the simulink parameters you set, we wrote', bytes_written,'bytes')
+print('These are the bytes we read (the test_code is not read)')
 for b in bytes_read:
 	print(b, end=' ')
