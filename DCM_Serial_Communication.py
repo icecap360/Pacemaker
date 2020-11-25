48ee33ff86e159898f04eafa5c8ba3622b0436f4
muhammad ali <muhammadaliog3@live.com>
2020-11-25 15:08:09 -0500

    ass2 update

--- DCM_Serial_Communication.py
+++ DCM_Serial_Communication.py
@@ -9,12 +9,12 @@ for p in list_ports.comports():
 		break
 port_name = port.device
 print('here')
-test_code, set_code, echo_code = 69,7,21
+test_code, set_code, echo_code = 10,20,30
 
 def echo_params():
 	### ECHO_PARAMS
 	with serial.Serial(port=port_name, baudrate=baudrate) as device:
-		dat = [test_code, echo_code] + [i for i in range(84)]
+		dat = [test_code, echo_code] + [i for i in range(20)]
 		dat = serial.to_bytes(dat)
 		bytes_written = device.write(dat)
 		print('done writing ECHO_PARAMS')
@@ -22,7 +22,7 @@ def echo_params():
 def set_params():
 	### SET_PARAMS
 	with serial.Serial(port=port_name, baudrate=baudrate) as device:	
-		dat = [test_code, set_code] + [i for i in range(84)]
+		dat = [test_code, set_code] + [i for i in range(20)]
 		dat = serial.to_bytes(dat)
 		bytes_written = device.write(dat)
 		print('SET_PARAMS complete, written ', bytes_written, 'bytes')
@@ -35,9 +35,9 @@ def read_params(n):
 		for byt in bytes_read:
 			print(byt)
 
-#set_params()
+set_params()
 #echo_params()
-read_params(1)
+#read_params(1)
 
 ###PROBLEMS
 
@@ -49,3 +49,6 @@ read_params(1)
 #Sometimes to actually get the serial port to read the new data we must read twice, if you read just once you get the old result, 
 #I tried adding some delay as well, but unfortunately that did not work 
 	
+
+
+
