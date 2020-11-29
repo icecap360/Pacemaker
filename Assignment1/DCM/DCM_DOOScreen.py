3850d39f8a1e12e1713870fe6ce75874c5b3f1df
vandej61 <72812297+vandej61@users.noreply.github.com>
2020-11-29 17:55:16 -0500

    Update DCM_DOOScreen.py

--- Assignment1/DCM/DCM_DOOScreen.py
+++ Assignment1/DCM/DCM_DOOScreen.py
@@ -4,57 +4,227 @@ import DCM_WelcomeScreen
 import threading
 import sys
 import time
+import serial
 from DCM_Check_Connection import checkConnection
+from DCM_Serial import echo_params
+from DCM_Serial_ju import read_params8
+from DCM_Serial_ju import read_params16
+from serial.tools import list_ports
+import struct
+port = None
+baudrate = 115200
+num_bytes_sent = 27
+for p in list_ports.comports():
+    if 'Link' in p.__str__():
+        port = p
+        break
+port_name = port.device
+test_code, set_code, echo_code = 10,20,30
+
+AVDelay=0
 
 def openDOO():
-    # Initialize variables
-    LowRL_F = open("LowRL.txt", "r")
-    try:
-        LowRL = LowRL_F.readline()
-    except:
-        LowRL = 60 #Default value
-    finally:
-        LowRL_F.close()
-        
-    UpRL_F = open("UpRL.txt", "r")
-    try:
-        UpRL = UpRL_F.readline()
-    except:
-        UpRL = 120 #Default value
-    finally:
-        UpRL_F.close()
+    global AVDelay
+    #read all params
+    #MCP
+    r1 = False
+    while (r1==False):
+        echo_params(1)
+        read_params8(2)
+        data = read_params8(2)
+        if (data[0]==1):
+            MCP = data[1]
+            r1 = True
 
-    AtrialAmp_F = open("AtrialAmp.txt", "r")
-    try:
-        AtrialAmp = AtrialAmp_F.readline()
-    except:
-        AtrialAmp = 3.5 #Default value
-    finally:
-        AtrialAmp_F.close()
-        
-    AtrialPW_F = open("AtrialPW.txt", "r")
-    try:
-        AtrialPW = AtrialPW_F.readline()
-    except:
-        AtrialPW = 0.4 #Default value
-    finally:
-        AtrialPW_F.close()
-        
-    VentAmp_F = open("VentAmp.txt", "r")
-    try:
-        VentAmp = VentAmp_F.readline()
-    except:
-        VentAmp = 3.5 #Default value
-    finally:
-        VentAmp_F.close()
-        
-    VentPW_F = open("VentPW.txt", "r")
-    try:
-        VentPW = VentPW_F.readline()
-    except:
-        VentPW = 0.4 #Default value
-    finally:
-        VentPW_F.close()
+    #MCS
+    r1 = False
+    while (r1==False):
+        echo_params(2)
+        read_params8(2)
+        data = read_params8(2)
+        if (data[0]==2):
+            MCS = data[1]
+            r1 = True
+
+    #MR
+    r1 = False
+    while (r1==False):
+        echo_params(3)
+        read_params8(2)
+        data = read_params8(2)
+        if (data[0]==3):
+            MR = data[1]
+            r1 = True
+
+    #VentAmp
+    r1 = False
+    while (r1==False):
+        echo_params(4)
+        read_params8(2)
+        data = read_params8(2)
+        if (data[0]==4):
+            VentAmp = data[1]
+            r1 = True
+
+    #VentPW
+    r1 = False
+    while (r1==False):
+        echo_params(5)
+        read_params8(2)
+        data = read_params8(2)
+        if (data[0]==5):
+            VentPW = data[1]
+            r1 = True
+
+    #VentSens
+    r1 = False
+    while (r1==False):
+        echo_params(6)
+        read_params8(2)
+        data = read_params8(2)
+        if (data[0]==6):
+            VentSens = data[1]
+            r1 = True
+
+    #AtrAmp
+    r1 = False
+    while (r1==False):
+        echo_params(7)
+        read_params8(2)
+        data = read_params8(2)
+        if (data[0]==7):
+            AtrAmp = data[1]
+            r1 = True
+
+    #AtrPW
+    r1 = False
+    while (r1==False):
+        echo_params(8)
+        read_params8(2)
+        data = read_params8(2)
+        if (data[0]==8):
+            AtrPW = data[1]
+            r1 = True
+
+    #AtrSens
+    r1 = False
+    while (r1==False):
+        echo_params(9)
+        read_params8(2)
+        data = read_params8(2)
+        if (data[0]==9):
+            AtrSens = data[1]
+            r1 = True
+
+    #Hyst
+    r1 = False
+    while (r1==False):
+        echo_params(10)
+        read_params8(2)
+        data = read_params8(2)
+        if (data[0]==10):
+            Hyst = data[1]
+            r1 = True
+
+    #LowRL
+    r1 = False
+    while (r1==False):
+        echo_params(11)
+        read_params16(3)
+        data = read_params16(3)
+        if (data[0]==11):
+            LowRL = data[1]
+            r1 = True
+
+    #AVDelay
+    r1 = False
+    while (r1==False):
+        echo_params(12)
+        read_params16(3)
+        data = read_params16(3)
+        if (data[0]==12):
+            AVDelay = data[1]
+        r1 = True
+
+    #VRP
+    r1 = False
+    while (r1==False):
+        echo_params(13)
+        read_params16(3)
+        data = read_params16(3)
+        if (data[0]==13):
+            VRP = data[1]
+            r1 = True
+
+    #ARP        
+    r1 = False
+    while (r1==False):
+        echo_params(14)
+        read_params16(3)
+        data = read_params16(3)
+        if (data[0]==14):
+            ARP = data[1]
+            r1 = True
+
+    #HEI
+    r1 = False
+    while (r1==False):
+        echo_params(15)
+        read_params16(3)
+        data = read_params16(3)
+        if (data[0]==15):
+            HEI = data[1]
+            r1 = True
+
+    #MaxSR
+    r1 = False
+    while (r1==False):
+        echo_params(16)
+        read_params8(2)
+        data = read_params8(2)
+        if (data[0]==16):
+            MaxSR = data[1]
+            r1 = True
+
+    #ModeAd
+    r1 = False
+    while (r1==False):
+        echo_params(17)
+        read_params8(2)
+        data = read_params8(2)
+        if (data[0]==17):
+            ModeAd = data[1]
+            r1 = True
+
+    #ReacTime
+    r1 = False
+    while (r1==False):
+        echo_params(18)
+        read_params8(2)
+        data = read_params8(2)
+        if (data[0]==18):
+            ReacTime = data[1]
+            r1 = True
+
+    #RespF
+    r1 = False
+    while (r1==False):
+        echo_params(19)
+        read_params8(2)
+        data = read_params8(2)
+        if (data[0]==19):
+            RespF = data[1]
+            r1 = True
+
+    #RecTime
+    r1 = False
+    while (r1==False):
+        echo_params(20)
+        read_params8(2)
+        data = read_params8(2)
+        if (data[0]==20):
+            RecTime = data[1]
+            r1 = True
 
     # Create AOO Page
     DOOPage = Toplevel()
@@ -65,20 +235,26 @@ def openDOO():
     DOOPageLabel= Label(DOOPage, text="DOO Pacing Mode", font=(None,20,'underline'))
 
     LowRL_L = Label(DOOPage, text = "Lower Rate Limit (ppm)", font =(None,12))
-    UpRL_L = Label(DOOPage, text = "Upper Rate Limit (ppm)", font =(None,12))
     AtrialAmp_L = Label(DOOPage, text = "Atrial Amplitude (V)", font =(None,12))
     AtrialPW_L = Label(DOOPage, text = "Atrial Pulse Width (ms)", font =(None,12))
     VentAmp_L = Label(DOOPage, text = "Ventricular Amplitude (V)", font =(None,12))
     VentPW_L = Label(DOOPage, text = "Ventricular Pulse Width (ms)", font =(None,12))
 
     LowRL_V = Label(DOOPage, text = LowRL, font =(None,12))
-    UpRL_V = Label(DOOPage, text = UpRL, font =(None,12))
-    AtrialAmp_V = Label(DOOPage, text = AtrialAmp, font =(None,12))
-    AtrialPW_V = Label(DOOPage, text = AtrialPW, font =(None,12))
+    AtrialAmp_V = Label(DOOPage, text = AtrAmp, font =(None,12))
+    AtrialPW_V = Label(DOOPage, text = AtrPW, font =(None,12))
     VentAmp_V = Label(DOOPage, text = VentAmp, font =(None,12))
     VentPW_V = Label(DOOPage, text = VentPW, font =(None,12))
 
     # Button Functions
+    def set_params():
+        with serial.Serial(port=port_name, baudrate=baudrate) as device:	
+            params = [MCP,MCS,MR,VentAmp,VentPW,VentSens,AtrAmp,AtrPW,AtrSens,Hyst,LowRL,AVDelay,VRP,ARP,HEI,MaxSR,ModeAd,ReacTime,RespF,RecTime]
+            #params = struct.pack("<"+"BBB"+"fBf"*2+"B"+"H"*5, *params)
+            params = struct.pack("<"+"B"*10+"H"*5+"B"*5, *params)
+            dat = serial.to_bytes([test_code, set_code]) + params
+            bytes_written = device.write(dat)
+            
     def changeLowRL():
         try:
             #check variable range
@@ -89,61 +265,36 @@ def openDOO():
                 LowRL_V.config(text = "Value too high")
             else:
                 LowRL_V.config(text = LowRL)
-                #write to file
-                LowRL_F = open("LowRL.txt", "w")
-                LowRL_F.write(LowRL_E.get())
-                LowRL_F.close()
+                set_params()
         except:
             LowRL_V.config(text = "Invalid Value")
 
-    def changeUpRL():
-        try:
-            #check variable range
-            UpRL = int(UpRL_E.get())
-            if (UpRL < 50):
-                UpRL_V.config(text = "Value too low")
-            elif (UpRL > 175):
-                UpRL_V.config(text = "Value too high")
-            else:
-                UpRL_V.config(text = UpRL)
-                #write to file
-                UpRL_F = open("UpRL.txt", "w")
-                UpRL_F.write(UpRL_E.get())
-                UpRL_F.close()
-        except:
-            UpRL_V.config(text = "Invalid Value")
-        
     def changeAtrAmp():
         try:
             #check variable range
-            AtrialAmp = float(AtrialAmp_E.get())
-            if (AtrialAmp < 0.5):
+            AtrAmp = float(AtrialAmp_E.get())
+            if (AtrAmp < 0.5):
                 AtrialAmp_V.config(text = "Value too low")
-            elif (AtrialAmp > 7.0):
+            elif (AtrAmp > 7.0):
                 AtrialAmp_V.config(text = "Value too high")
             else:
-                AtrialAmp_V.config(text = AtrialAmp)
-                #write to file
-                AtrialAmp_F = open("AtrialAmp.txt", "w")
-                AtrialAmp_F.write(AtrialAmp_E.get())
-                AtrialAmp_F.close()
+                AtrialAmp_V.config(text = AtrAmp)
+                AtrAmp = AtrAmp*10
+                set_params()
         except:
             AtrialAmp_V.config(text = "Invalid Value")
         
     def changeAtrPW():
         try:
             #check variable range
-            AtrialPW = float(AtrialPW_E.get())
-            if (AtrialPW < 0.05):
+            AtrPW = float(AtrialPW_E.get())
+            if (AtrPW < 0.05):
                 AtrialPW_V.config(text = "Value too low")
-            elif (AtrialPW > 1.9):
+            elif (AtrPW > 1.9):
                 AtrialPW_V.config(text = "Value too high")
             else:
-                AtrialPW_V.config(text = AtrialPW)
-                #write to file
-                AtrialPW_F = open("AtrialPW.txt", "w")
-                AtrialPW_F.write(AtrialPW_E.get())
-                AtrialPW_F.close()
+                AtrialPW_V.config(text = AtrPW)
+                set_params()
         except:
             AtrialPW_V.config(text = "Invalid Value")
     
@@ -157,10 +308,8 @@ def openDOO():
                 VentAmp_V.config(text = "Value too high")
             else:
                 VentAmp_V.config(text = VentAmp)
-                #write to file
-                VentAmp_F = open("VentAmp.txt", "w")
-                VentAmp_F.write(VentAmp_E.get())
-                VentAmp_F.close()
+                VentAmp = VentAmp*10
+                set_params()
         except:
             VentAmp_V.config(text = "Invalid Value")
             
@@ -174,19 +323,13 @@ def openDOO():
                 VentPW_V.config(text = "Value too high")
             else:
                 VentPW_V.config(text = VentPW)
-                #write to file
-                VentPW_F = open("VentPW.txt", "w")
-                VentPW_F.write(VentPW_E.get())
-                VentPW_F.close()
+                set_params()
         except:
             VentPW_V.config(text = "Invalid Value")
 
     # Create Entries
     LowRL_E = Entry(DOOPage, width=20)
     LowRL_E.insert(0, "Enter New Value")
-
-    UpRL_E = Entry(DOOPage, width=20)
-    UpRL_E.insert(0, "Enter New Value")
 
     AtrialAmp_E = Entry(DOOPage, width=20)
     AtrialAmp_E.insert(0, "Enter New Value")
@@ -202,7 +345,6 @@ def openDOO():
 
     # Create Buttons
     LowRL_B = Button(DOOPage, text="Update", command=changeLowRL)
-    UpRL_B = Button(DOOPage, text="Update", command=changeUpRL)
     AtrialAmp_B = Button(DOOPage, text="Update", command=changeAtrAmp)
     AtrialPW_B = Button(DOOPage, text="Update", command=changeAtrPW)
     VentAmp_B = Button(DOOPage, text="Update", command=changeVentAmp)
@@ -215,31 +357,26 @@ def openDOO():
     LowRL_V.grid(row= 2, column= 1)
     LowRL_E.grid(row= 2, column= 2)
     LowRL_B.grid(row= 2, column= 3)
+    
+    AtrialAmp_L.grid(row= 3, column= 0)
+    AtrialAmp_V.grid(row= 3, column= 1)
+    AtrialAmp_E.grid(row= 3, column= 2)
+    AtrialAmp_B.grid(row= 3, column= 3)
 
-    UpRL_L.grid(row= 3, column= 0)
-    UpRL_V.grid(row= 3, column= 1)
-    UpRL_E.grid(row= 3, column= 2)
-    UpRL_B.grid(row= 3, column= 3)
+    AtrialPW_L.grid(row= 4, column= 0)
+    AtrialPW_V.grid(row= 4, column= 1)
+    AtrialPW_E.grid(row= 4, column= 2)
+    AtrialPW_B.grid(row= 4, column= 3)
 
-    AtrialAmp_L.grid(row= 4, column= 0)
-    AtrialAmp_V.grid(row= 4, column= 1)
-    AtrialAmp_E.grid(row= 4, column= 2)
-    AtrialAmp_B.grid(row= 4, column= 3)
+    VentAmp_L.grid(row= 5, column= 0)
+    VentAmp_V.grid(row= 5, column= 1)
+    VentAmp_E.grid(row= 5, column= 2)
+    VentAmp_B.grid(row= 5, column= 3)
 
-    AtrialPW_L.grid(row= 5, column= 0)
-    AtrialPW_V.grid(row= 5, column= 1)
-    AtrialPW_E.grid(row= 5, column= 2)
-    AtrialPW_B.grid(row= 5, column= 3)
-
-    VentAmp_L.grid(row= 6, column= 0)
-    VentAmp_V.grid(row= 6, column= 1)
-    VentAmp_E.grid(row= 6, column= 2)
-    VentAmp_B.grid(row= 6, column= 3)
-
-    VentPW_L.grid(row= 7, column= 0)
-    VentPW_V.grid(row= 7, column= 1)
-    VentPW_E.grid(row= 7, column= 2)
-    VentPW_B.grid(row= 7, column= 3)
+    VentPW_L.grid(row= 6, column= 0)
+    VentPW_V.grid(row= 6, column= 1)
+    VentPW_E.grid(row= 6, column= 2)
+    VentPW_B.grid(row= 6, column= 3)
 
 
     #Statues Bar
