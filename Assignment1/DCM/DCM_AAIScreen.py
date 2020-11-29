085cfbcf191b764126929845dce74a0ce54ff1f5
vandej61 <72812297+vandej61@users.noreply.github.com>
2020-11-29 15:56:03 -0500

    Update DCM_AAIScreen.py

--- Assignment1/DCM/DCM_AAIScreen.py
+++ Assignment1/DCM/DCM_AAIScreen.py
@@ -1,85 +1,231 @@
-#AII Pacing
+#AAI Pacing
 from tkinter import *
 import DCM_WelcomeScreen
 import threading
 import sys
 import time
+import serial
 from DCM_Check_Connection import checkConnection
+from DCM_Serial import echo_params
+from DCM_Serial_ju import read_params8
+from DCM_Serial_ju import read_params16
+from serial.tools import list_ports
+import struct
+port = None
+baudrate = 115200
+num_bytes_sent = 27
+for p in list_ports.comports():
+    if 'Link' in p.__str__():
+        port = p
+        break
+port_name = port.device
+test_code, set_code, echo_code = 10,20,30
+
+AVDelay=0
 
 def openAAI():
-    # Variables needed
-    LowRL_F = open("LowRL.txt", "r")
-    try:
-        LowRL = LowRL_F.readline()
-    except:
-        LowRL = 60 #Default value
-    finally:
-        LowRL_F.close()
-        
-    UpRL_F = open("UpRL.txt", "r")
-    try:
-        UpRL = UpRL_F.readline()
-    except:
-        UpRL = 120 #Default value
-    finally:
-        UpRL_F.close()
+    global AVDelay
+    #read all params
+    #MCP
+    r1 = False
+    while (r1==False):
+        echo_params(1)
+        read_params8(2)
+        data = read_params8(2)
+        if (data[0]==1):
+            MCP = data[1]
+            r1 = True
 
-    AtrAmp_F = open("AtrialAmp.txt", "r")
-    try:
-        AtrAmp = AtrAmp_F.readline()
-    except:
-        AtrAmp = 3.5 #Default value
-    finally:
-        AtrAmp_F.close()
-        
-    AtrPW_F = open("AtrialPW.txt", "r")
-    try:
-        AtrPW = AtrPW_F.readline()
-    except:
-        AtrPW = 0.4 #Default value
-    finally:
-        AtrPW_F.close()
+    #MCS
+    r1 = False
+    while (r1==False):
+        echo_params(2)
+        read_params8(2)
+        data = read_params8(2)
+        if (data[0]==2):
+            MCS = data[1]
+            r1 = True
 
-    AtrSens_F = open("AtrSens.txt", "r")
-    try:
-        AtrSens = AtrSens_F.readline()
-    except:
-        AtrSens = 2.5 #Default value
-    finally:
-        AtrSens_F.close()
+    #MR
+    r1 = False
+    while (r1==False):
+        echo_params(3)
+        read_params8(2)
+        data = read_params8(2)
+        if (data[0]==3):
+            MR = data[1]
+            r1 = True
 
-    ARP_F = open("ARP.txt", "r")
-    try:
-        ARP = ARP_F.readline()
-    except:
-        ARP = 250 #Default value
-    finally:
-        ARP_F.close()
+    #VentAmp
+    r1 = False
+    while (r1==False):
+        echo_params(4)
+        read_params8(2)
+        data = read_params8(2)
+        if (data[0]==4):
+            VentAmp = data[1]
+            r1 = True
 
-    PVARP_F = open("PVARP.txt", "r")
-    try:
-        PVARP = PVARP_F.readline()
-    except:
-        PVARP = 250 #Default value
-    finally:
-        PVARP_F.close()
-        
-    RateSmo_F = open("RateSmo.txt", "r")
-    try:
-        RateSmo = RateSmo_F.readline()
-    except:
-        RateSmo = 3 #Default value
-    finally:
-        RateSmo_F.close()
+    #VentPW
+    r1 = False
+    while (r1==False):
+        echo_params(5)
+        read_params8(2)
+        data = read_params8(2)
+        if (data[0]==5):
+            VentPW = data[1]
+            r1 = True
 
-    Hyst_F = open("Hyst.txt", "r")
-    try:
-        Hyst = int(Hyst_F.readline())
-    except:
-        Hyst = 0 #Default value
-    finally:
-        Hyst_F.close()
+    #VentSens
+    r1 = False
+    while (r1==False):
+        echo_params(6)
+        read_params8(2)
+        data = read_params8(2)
+        if (data[0]==6):
+            VentSens = data[1]
+            r1 = True
 
+    #AtrAmp
+    r1 = False
+    while (r1==False):
+        echo_params(7)
+        read_params8(2)
+        data = read_params8(2)
+        if (data[0]==7):
+            AtrAmp = data[1]
+            r1 = True
+
+    #AtrPW
+    r1 = False
+    while (r1==False):
+        echo_params(8)
+        read_params8(2)
+        data = read_params8(2)
+        if (data[0]==8):
+            AtrPW = data[1]
+            r1 = True
+
+    #AtrSens
+    r1 = False
+    while (r1==False):
+        echo_params(9)
+        read_params8(2)
+        data = read_params8(2)
+        if (data[0]==9):
+            AtrSens = data[1]
+            r1 = True
+
+    #Hyst
+    r1 = False
+    while (r1==False):
+        echo_params(10)
+        read_params8(2)
+        data = read_params8(2)
+        if (data[0]==10):
+            Hyst = data[1]
+            r1 = True
+
+    #LowRL
+    r1 = False
+    while (r1==False):
+        echo_params(11)
+        read_params16(3)
+        data = read_params16(3)
+        if (data[0]==11):
+            LowRL = data[1]
+            r1 = True
+
+    #AVDelay
+    r1 = False
+    while (r1==False):
+        echo_params(12)
+        read_params16(3)
+        data = read_params16(3)
+        if (data[0]==12):
+            AVDelay = data[1]
+        r1 = True
+
+    #VRP
+    r1 = False
+    while (r1==False):
+        echo_params(13)
+        read_params16(3)
+        data = read_params16(3)
+        if (data[0]==13):
+            VRP = data[1]
+            r1 = True
+
+    #ARP        
+    r1 = False
+    while (r1==False):
+        echo_params(14)
+        read_params16(3)
+        data = read_params16(3)
+        if (data[0]==14):
+            ARP = data[1]
+            r1 = True
+
+    #HEI
+    r1 = False
+    while (r1==False):
+        echo_params(15)
+        read_params16(3)
+        data = read_params16(3)
+        if (data[0]==15):
+            HEI = data[1]
+            r1 = True
+
+    #MaxSR
+    r1 = False
+    while (r1==False):
+        echo_params(16)
+        read_params8(2)
+        data = read_params8(2)
+        if (data[0]==16):
+            MaxSR = data[1]
+            r1 = True
+
+    #ModeAd
+    r1 = False
+    while (r1==False):
+        echo_params(17)
+        read_params8(2)
+        data = read_params8(2)
+        if (data[0]==17):
+            ModeAd = data[1]
+            r1 = True
+
+    #ReacTime
+    r1 = False
+    while (r1==False):
+        echo_params(18)
+        read_params8(2)
+        data = read_params8(2)
+        if (data[0]==18):
+            ReacTime = data[1]
+            r1 = True
+
+    #RespF
+    r1 = False
+    while (r1==False):
+        echo_params(19)
+        read_params8(2)
+        data = read_params8(2)
+        if (data[0]==19):
+            RespF = data[1]
+            r1 = True
+
+    #RecTime
+    r1 = False
+    while (r1==False):
+        echo_params(20)
+        read_params8(2)
+        data = read_params8(2)
+        if (data[0]==20):
+            RecTime = data[1]
+            r1 = True
+    
     # Create AAI Page
     AIIpage = Toplevel()
     AIIpage.title("AAI Pacing Mode")
@@ -89,62 +235,44 @@ def openAAI():
     AIIpageLabel= Label(AIIpage, text="AAI Pacing Mode", font=(None,20,'underline'))
 
     LowRL_L = Label(AIIpage, text = "Lower Rate Limit (ppm)", font =(None,12))
-    UpRL_L = Label(AIIpage, text = "Upper Rate Limit (ppm)", font =(None,12))
     AtrAmp_L = Label(AIIpage, text = "Atrial Amplitude (V)", font =(None,12))
     AtrPW_L = Label(AIIpage, text = "Atrial Pulse Width (ms)", font =(None,12))
 
     LowRL_V = Label(AIIpage, text = LowRL, font =(None,12))
-    UpRL_V = Label(AIIpage, text = UpRL, font =(None,12))
     AtrAmp_V = Label(AIIpage, text = AtrAmp, font =(None,12))
     AtrPW_V = Label(AIIpage, text = AtrPW, font =(None,12))
 
     #Additional Labels
     AtrSens_L = Label(AIIpage, text = "Atrial Sensitivity (mV)", font =(None,12))
     ARP_L = Label(AIIpage, text = "Refractory Period (ms)", font =(None,12))
-    PVARP_L = Label(AIIpage, text = "PVA Refractory Period (ms)", font =(None,12))
-    RateSmo_L = Label(AIIpage, text = "Rate Smoothing", font =(None,12))
     Hyst_L = Label(AIIpage, text = "Hysteresis", font =(None,12))
 
     AtrSens_V = Label(AIIpage, text = AtrSens, font =(None,12))
     ARP_V = Label(AIIpage, text = ARP, font =(None,12))
-    PVARP_V = Label(AIIpage, text = PVARP, font =(None,12))
-    RateSmo_V = Label(AIIpage, text = RateSmo, font =(None,12))
     Hyst_V = Label(AIIpage, text = "Disabled", font =(None,12))
 
     # Button Functions
+    def set_params():
+        with serial.Serial(port=port_name, baudrate=baudrate) as device:	
+            params = [MCP,MCS,MR,VentAmp,VentPW,VentSens,AtrAmp,AtrPW,AtrSens,Hyst,LowRL,AVDelay,VRP,ARP,HEI,MaxSR,ModeAd,ReacTime,RespF,RecTime]
+            #params = struct.pack("<"+"BBB"+"fBf"*2+"B"+"H"*5, *params)
+            params = struct.pack("<"+"B"*10+"H"*5+"B"*5, *params)
+            dat = serial.to_bytes([test_code, set_code]) + params
+            bytes_written = device.write(dat)
+                    
     def changeLowRL():
-        try:
-            #check variable range
-            LowRL = int(LowRL_E.get())
-            if (LowRL < 30):
-                LowRL_V.config(text = "Value too low")
-            elif (LowRL > 175):
-                LowRL_V.config(text = "Value too high")
-            else:
-                LowRL_V.config(text = LowRL)
-                #write to file
-                LowRL_F = open("LowRL.txt", "w")
-                LowRL_F.write(LowRL_E.get())
-                LowRL_F.close()
+        #try:
+        #check variable range
+        LowRL = int(LowRL_E.get())
+        if (LowRL < 30):
+            LowRL_V.config(text = "Value too low")
+        elif (LowRL > 175):
+            LowRL_V.config(text = "Value too high")
+        else:
+            LowRL_V.config(text = LowRL)
+            set_params()
         except:
             LowRL_V.config(text = "Invalid Value")
-
-    def changeUpRL():
-        try:
-            #check variable range
-            UpRL = int(UpRL_E.get())
-            if (UpRL < 50):
-                UpRL_V.config(text = "Value too low")
-            elif (UpRL > 175):
-                UpRL_V.config(text = "Value too high")
-            else:
-                UpRL_V.config(text = UpRL)
-                #write to file
-                UpRL_F = open("UpRL.txt", "w")
-                UpRL_F.write(UpRL_E.get())
-                UpRL_F.close()
-        except:
-            UpRL_V.config(text = "Invalid Value")
         
     def changeAtrAmp():
         try:
@@ -156,10 +284,8 @@ def openAAI():
                 AtrAmp_V.config(text = "Value too high")
             else:
                 AtrAmp_V.config(text = AtrAmp)
-                #write to file
-                AtrAmp_F = open("AtrialAmp.txt", "w")
-                AtrAmp_F.write(AtrAmp_E.get())
-                AtrAmp_F.close()
+                AtrAmp = AtrAmp*10
+                set_params()
         except:
             AtrialAmp_V.config(text = "Invalid Value")
         
@@ -173,10 +299,7 @@ def openAAI():
                 AtrPW_V.config(text = "Value too high")
             else:
                 AtrPW_V.config(text = AtrPW)
-                #write to file
-                AtrPW_F = open("AtrialPW.txt", "w")
-                AtrPW_F.write(AtrPW_E.get())
-                AtrPW_F.close()
+                set_params()
         except:
             AtrPW_V.config(text = "Invalid Value")
 
@@ -192,10 +315,9 @@ def openAAI():
                 AtrSens_V.config(text = "Value too high")
             else:
                 AtrSens_V.config(text = AtrSens)
-                #write to file
-                AtrSens_F = open("AtrSens.txt", "w")
-                AtrSens_F.write(AtrSens_E.get())
-                AtrSens_F.close()
+                AtrSens = AtrSens*10
+                set_params()
+                
         except:
             AtrSens_V.config(text = "Invalid Value")
 
@@ -209,69 +331,24 @@ def openAAI():
                 ARP_V.config(text = "Value too high")
             else:
                 ARP_V.config(text = ARP)
-                #write to file
-                ARP_F = open("ARP.txt", "w")
-                ARP_F.write(ARP_E.get())
-                ARP_F.close()
+                set_params()
         except:
             ARP_V.config(text = "Invalid Value")
-
-    def changePVARP():
-        try:
-            #check variable range
-            PVARP = int(PVARP_E.get())
-            if (PVARP < 150):
-                PVARP_V.config(text = "Value too low")
-            elif (PVARP > 500):
-                PVARP_V.config(text = "Value too high")
-            else:
-                PVARP_V.config(text = PVARP)
-                #write to file
-                PVARP_F = open("PVARP.txt", "w")
-                PVARP_F.write(PVARP_E.get())
-                PVARP_F.close()
-        except:
-            PVARP_V.config(text = "Invalid Value")
-            
-    def changeRateSmo():
-        try:
-            #check variable range
-            RateSmo = int(RateSmo_E.get())
-            if (RateSmo < 0):
-                RateSmo_V.config(text = "Value too low")
-            elif (RateSmo > 21):
-                RateSmo_V.config(text = "Value too high")
-            else:
-                RateSmo_V.config(text = RateSmo)
-                #write to file
-                RateSmo_F = open("RateSmo.txt", "w")
-                RateSmo_F.write(RateSmo_E.get())
-                RateSmo_F.close()
-        except:
-            RateSmo_V.config(text = "Invalid Value")
 
     def changeHyst():
         global Hyst
         if (Hyst):
             Hyst = 0
             Hyst_V.config(text="Disabled")
-            Hyst_F = open("Hyst.txt", "w")
-            Hyst_F.write("0")
-            Hyst_F.close()
+            set_params()
         else:
             Hyst = 1
             Hyst_V.config(text="Enabled")
-            Hyst_F = open("Hyst.txt", "w")
-            Hyst_F.write("1")
-            Hyst_F.close()
-
+            set_params
 
     # Create Entries
     LowRL_E = Entry(AIIpage, width=20)
     LowRL_E.insert(0, "Enter New Value")
-
-    UpRL_E = Entry(AIIpage, width=20)
-    UpRL_E.insert(0, "Enter New Value")
 
     AtrAmp_E = Entry(AIIpage, width=20)
     AtrAmp_E.insert(0, "Enter New Value")
@@ -286,22 +363,13 @@ def openAAI():
     ARP_E = Entry(AIIpage, width=20)
     ARP_E.insert(0, "Enter New Value")
 
-    PVARP_E = Entry(AIIpage, width=20)
-    PVARP_E.insert(0, "Enter New Value")
-
-    RateSmo_E = Entry(AIIpage, width=20)
-    RateSmo_E.insert(0, "Enter New Value")
-
     # Create Buttons
     LowRL_B = Button(AIIpage, text="Update", command=changeLowRL)
-    UpRL_B = Button(AIIpage, text="Update", command=changeUpRL)
     AtrAmp_B = Button(AIIpage, text="Update", command=changeAtrAmp)
     AtrPW_B = Button(AIIpage, text="Update", command=changeAtrPW)
 
     AtrSens_B = Button(AIIpage, text="Update", command=changeAtrSens)
     ARP_B = Button(AIIpage, text="Update", command=changeARP)
-    PVARP_B = Button(AIIpage, text="Update", command=changePVARP)
-    RateSmo_B = Button(AIIpage, text="Update", command=changeRateSmo)
     Hyst_B = Button(AIIpage, text="Toggle", command=changeHyst)
 
     # Organize objects
@@ -312,59 +380,45 @@ def openAAI():
     LowRL_E.grid(row= 2, column= 2)
     LowRL_B.grid(row= 2, column= 3)
 
-    UpRL_L.grid(row= 3, column= 0)
-    UpRL_V.grid(row= 3, column= 1)
-    UpRL_E.grid(row= 3, column= 2)
-    UpRL_B.grid(row= 3, column= 3)
+    AtrAmp_L.grid(row= 3, column= 0)
+    AtrAmp_V.grid(row= 3, column= 1)
+    AtrAmp_E.grid(row= 3, column= 2)
+    AtrAmp_B.grid(row= 3, column= 3)
 
-    AtrAmp_L.grid(row= 4, column= 0)
-    AtrAmp_V.grid(row= 4, column= 1)
-    AtrAmp_E.grid(row= 4, column= 2)
-    AtrAmp_B.grid(row= 4, column= 3)
-
-    AtrPW_L.grid(row= 5, column= 0)
-    AtrPW_V.grid(row= 5, column= 1)
-    AtrPW_E.grid(row= 5, column= 2)
-    AtrPW_B.grid(row= 5, column= 3)
+    AtrPW_L.grid(row= 4, column= 0)
+    AtrPW_V.grid(row= 4, column= 1)
+    AtrPW_E.grid(row= 4, column= 2)
+    AtrPW_B.grid(row= 4, column= 3)
 
     #Additional objects
 
-    AtrSens_L.grid(row= 6, column= 0)
-    AtrSens_V.grid(row= 6, column= 1)
-    AtrSens_E.grid(row= 6, column= 2)
-    AtrSens_B.grid(row= 6, column= 3)
+    AtrSens_L.grid(row= 5, column= 0)
+    AtrSens_V.grid(row= 5, column= 1)
+    AtrSens_E.grid(row= 5, column= 2)
+    AtrSens_B.grid(row= 5, column= 3)
 
-    ARP_L.grid(row= 7, column= 0)
-    ARP_V.grid(row= 7, column= 1)
-    ARP_E.grid(row= 7, column= 2)
-    ARP_B.grid(row= 7, column= 3)
+    ARP_L.grid(row= 6, column= 0)
+    ARP_V.grid(row= 6, column= 1)
+    ARP_E.grid(row= 6, column= 2)
+    ARP_B.grid(row= 6, column= 3)
 
-    PVARP_L.grid(row= 8, column= 0)
-    PVARP_V.grid(row= 8, column= 1)
-    PVARP_E.grid(row= 8, column= 2)
-    PVARP_B.grid(row= 8, column= 3)
-
-    RateSmo_L.grid(row= 9, column= 0)
-    RateSmo_V.grid(row= 9, column= 1)
-    RateSmo_E.grid(row= 9, column= 2)
-    RateSmo_B.grid(row= 9, column= 3)
     #Hyst
-    Hyst_L.grid(row= 10, column= 0)
-    Hyst_V.grid(row= 10, column= 1)
-    Hyst_B.grid(row= 10, column= 2)
+    Hyst_L.grid(row= 7, column= 0)
+    Hyst_V.grid(row= 7, column= 1)
+    Hyst_B.grid(row= 7, column= 2)
 
     #Statues Bar
- 
+
     def statusCreate():
-            while(True):
-                try:
-                    user=open("currentUser.txt","r")
-                    connectionStatus=checkConnection()
-                    statusLabel= Label(AIIpage, text="User: "+ user.read() +"            Connection Status: "+connectionStatus+"                ")
-                    statusLabel.place(relx=0.0, rely=1.0, anchor="sw")
-                    time.sleep(5000)
-                except TclError:
-                    sys.exit()
+        while(True):
+            try:
+                user=open("currentUser.txt","r")
+                connectionStatus=checkConnection()
+                statusLabel= Label(AIIpage, text="User: "+ user.read() +"            Connection Status: "+connectionStatus+"                ")
+                statusLabel.place(relx=0.0, rely=1.0, anchor="sw")
+                time.sleep(5000)
+            except TclError:
+                sys.exit()
 
 
     statusBar = threading.Thread(target=statusCreate, args=())
@@ -372,3 +426,5 @@ def openAAI():
     statusBar.start()
 
     mainloop()
+
+openAAI()
