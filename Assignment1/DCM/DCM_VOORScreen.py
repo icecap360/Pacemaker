912df1c6bab228e94bb51def61e380889debca54
vandej61 <72812297+vandej61@users.noreply.github.com>
2020-11-29 18:16:06 -0500

    Update DCM_VOORScreen.py

--- Assignment1/DCM/DCM_VOORScreen.py
+++ Assignment1/DCM/DCM_VOORScreen.py
@@ -4,73 +4,228 @@ import DCM_WelcomeScreen
 import threading
 import sys
 import time
+import serial
 from DCM_Check_Connection import checkConnection
+from DCM_Serial import echo_params
+from DCM_Serial_ju import read_params8
+from DCM_Serial_ju import read_params16
+from serial.tools import list_ports
+import struct
+port = None
+baudrate = 115200
+num_bytes_sent = 27
+for p in list_ports.comports():
+    if 'Link' in p.__str__():
+        port = p
+        break
+port_name = port.device
+test_code, set_code, echo_code = 10,20,30
+
+AVDelay=0
 
 def openVOOR():
     # Initialize variables
-    LowRL_F = open("LowRL.txt", "r")
-    try:
-        LowRL = LowRL_F.readline()
-    except:
-        LowRL = 60 #Default value
-    finally:
-        LowRL_F.close()
-        
-    UpRL_F = open("UpRL.txt", "r")
-    try:
-        UpRL = UpRL_F.readline()
-    except:
-        UpRL = 120 #Default value
-    finally:
-        UpRL_F.close()
+    global AVDelay
+    #read all params
+    #MCP
+    r1 = False
+    while (r1==False):
+        echo_params(1)
+        read_params8(2)
+        data = read_params8(2)
+        if (data[0]==1):
+            MCP = data[1]
+            r1 = True
 
-    VentAmp_F = open("VentAmp.txt", "r")
-    try:
-        VentAmp = VentAmp_F.readline()
-    except:
-        VentAmp = 3.5 #Default value
-    finally:
-        VentAmp_F.close()
-        
-    VentPW_F = open("VentPW.txt", "r")
-    try:
-        VentPW = VentPW_F.readline()
-    except:
-        VentPW = 0.4 #Default value
-    finally:
-        VentPW_F.close()
+    #MCS
+    r1 = False
+    while (r1==False):
+        echo_params(2)
+        read_params8(2)
+        data = read_params8(2)
+        if (data[0]==2):
+            MCS = data[1]
+            r1 = True
 
-    maxSensorRate_F = open("maxSensorRate.txt", "r")
-    try:
-        maxSensorRate = maxSensorRate_F.readline()
-    except:
-        maxSensorRate = 120 #Default value
-    finally:
-        maxSensorRate_F.close()
+    #MR
+    r1 = False
+    while (r1==False):
+        echo_params(3)
+        read_params8(2)
+        data = read_params8(2)
+        if (data[0]==3):
+            MR = data[1]
+            r1 = True
 
-    reactTime_F = open("reactTime.txt", "r")
-    try:
-        reactTime = reactTime_F.readline()
-    except:
-        reactTime = 30 #Default value
-    finally:
-        reactTime_F.close()
+    #VentAmp
+    r1 = False
+    while (r1==False):
+        echo_params(4)
+        read_params8(2)
+        data = read_params8(2)
+        if (data[0]==4):
+            VentAmp = data[1]
+            r1 = True
 
-    respFactor_F = open("respFactor.txt", "r")
-    try:
-        respFactor = respFactor_F.readline()
-    except:
-        respFactor = 8 #Default value
-    finally:
-        respFactor_F.close()
+    #VentPW
+    r1 = False
+    while (r1==False):
+        echo_params(5)
+        read_params8(2)
+        data = read_params8(2)
+        if (data[0]==5):
+            VentPW = data[1]
+            r1 = True
 
-    recoveryTime_F = open("recoveryTime.txt", "r")
-    try:
-        recoveryTime = recoveryTime_F.readline()
-    except:
-        recoveryTime = 5 #Default value
-    finally:
-        recoveryTime_F.close()
+    #VentSens
+    r1 = False
+    while (r1==False):
+        echo_params(6)
+        read_params8(2)
+        data = read_params8(2)
+        if (data[0]==6):
+            VentSens = data[1]
+            r1 = True
+
+    #AtrAmp
+    r1 = False
+    while (r1==False):
+        echo_params(7)
+        read_params8(2)
+        data = read_params8(2)
+        if (data[0]==7):
+            AtrAmp = data[1]
+            r1 = True
+
+    #AtrPW
+    r1 = False
+    while (r1==False):
+        echo_params(8)
+        read_params8(2)
+        data = read_params8(2)
+        if (data[0]==8):
+            AtrPW = data[1]
+            r1 = True
+
+    #AtrSens
+    r1 = False
+    while (r1==False):
+        echo_params(9)
+        read_params8(2)
+        data = read_params8(2)
+        if (data[0]==9):
+            AtrSens = data[1]
+            r1 = True
+
+    #Hyst
+    r1 = False
+    while (r1==False):
+        echo_params(10)
+        read_params8(2)
+        data = read_params8(2)
+        if (data[0]==10):
+            Hyst = data[1]
+            r1 = True
+
+    #LowRL
+    r1 = False
+    while (r1==False):
+        echo_params(11)
+        read_params16(3)
+        data = read_params16(3)
+        if (data[0]==11):
+            LowRL = data[1]
+            r1 = True
+
+    #AVDelay
+    r1 = False
+    while (r1==False):
+        echo_params(12)
+        read_params16(3)
+        data = read_params16(3)
+        if (data[0]==12):
+            AVDelay = data[1]
+        r1 = True
+
+    #VRP
+    r1 = False
+    while (r1==False):
+        echo_params(13)
+        read_params16(3)
+        data = read_params16(3)
+        if (data[0]==13):
+            VRP = data[1]
+            r1 = True
+
+    #ARP        
+    r1 = False
+    while (r1==False):
+        echo_params(14)
+        read_params16(3)
+        data = read_params16(3)
+        if (data[0]==14):
+            ARP = data[1]
+            r1 = True
+
+    #HEI
+    r1 = False
+    while (r1==False):
+        echo_params(15)
+        read_params16(3)
+        data = read_params16(3)
+        if (data[0]==15):
+            HEI = data[1]
+            r1 = True
+
+    #MaxSR
+    r1 = False
+    while (r1==False):
+        echo_params(16)
+        read_params8(2)
+        data = read_params8(2)
+        if (data[0]==16):
+            MaxSR = data[1]
+            r1 = True
+
+    #ModeAd
+    r1 = False
+    while (r1==False):
+        echo_params(17)
+        read_params8(2)
+        data = read_params8(2)
+        if (data[0]==17):
+            ModeAd = data[1]
+            r1 = True
+
+    #ReacTime
+    r1 = False
+    while (r1==False):
+        echo_params(18)
+        read_params8(2)
+        data = read_params8(2)
+        if (data[0]==18):
+            ReacTime = data[1]
+            r1 = True
+
+    #RespF
+    r1 = False
+    while (r1==False):
+        echo_params(19)
+        read_params8(2)
+        data = read_params8(2)
+        if (data[0]==19):
+            RespF = data[1]
+            r1 = True
+
+    #RecTime
+    r1 = False
+    while (r1==False):
+        echo_params(20)
+        read_params8(2)
+        data = read_params8(2)
+        if (data[0]==20):
+            RecTime = data[1]
+            r1 = True
     
     # Create AOO Page
     VOORPage = Toplevel()
@@ -81,7 +236,6 @@ def openVOOR():
     VOORPageLabel= Label(VOORPage, text="VOOR Pacing Mode", font=(None,20,'underline'))
 
     LowRL_L = Label(VOORPage, text = "Lower Rate Limit (ppm)", font =(None,12))
-    UpRL_L = Label(VOORPage, text = "Upper Rate Limit (ppm)", font =(None,12))
     VentAmp_L = Label(VOORPage, text = "Ventricular Amplitude (V)", font =(None,12))
     VentPW_L = Label(VOORPage, text = "Ventricular Pulse Width (ms)", font =(None,12))
     maxSensorRate_L= Label(VOORPage, text="Maximum Sensor Rate (ppm)", font=(None,12))
@@ -90,15 +244,22 @@ def openVOOR():
     recoveryTime_L= Label(VOORPage, text="Recovery Time (min)", font=(None,12))
 
     LowRL_V = Label(VOORPage, text = LowRL, font =(None,12))
-    UpRL_V = Label(VOORPage, text = UpRL, font =(None,12))
     VentAmp_V = Label(VOORPage, text = VentAmp, font =(None,12))
     VentPW_V = Label(VOORPage, text = VentPW, font =(None,12))
-    maxSensorRate_V= Label(VOORPage, text=maxSensorRate, font=(None,12))
-    reactTime_V= Label(VOORPage, text=reactTime, font=(None,12))
-    respFactor_V= Label(VOORPage, text=respFactor, font=(None,12))
-    recoveryTime_V= Label(VOORPage, text=recoveryTime, font=(None,12))
+    maxSensorRate_V= Label(VOORPage, text=MaxSR, font=(None,12))
+    reactTime_V= Label(VOORPage, text=ReacTime, font=(None,12))
+    respFactor_V= Label(VOORPage, text=RespF, font=(None,12))
+    recoveryTime_V= Label(VOORPage, text=RecTime, font=(None,12))
 
     # Button Functions
+    def set_params():
+        with serial.Serial(port=port_name, baudrate=baudrate) as device:	
+            params = [MCP,MCS,MR,VentAmp,VentPW,VentSens,AtrAmp,AtrPW,AtrSens,Hyst,LowRL,AVDelay,VRP,ARP,HEI,MaxSR,ModeAd,ReacTime,RespF,RecTime]
+            #params = struct.pack("<"+"BBB"+"fBf"*2+"B"+"H"*5, *params)
+            params = struct.pack("<"+"B"*10+"H"*5+"B"*5, *params)
+            dat = serial.to_bytes([test_code, set_code]) + params
+            bytes_written = device.write(dat)
+            
     def changeLowRL():
         try:
             #check variable range
@@ -109,29 +270,9 @@ def openVOOR():
                 LowRL_V.config(text = "Value too high")
             else:
                 LowRL_V.config(text = LowRL)
-                #write to file
-                LowRL_F = open("LowRL.txt", "w")
-                LowRL_F.write(LowRL_E.get())
-                LowRL_F.close()
+                set_params()
         except:
             LowRL_V.config(text = "Invalid Value")
-
-    def changeUpRL():
-        try:
-            #check variable range
-            UpRL = int(UpRL_E.get())
-            if (UpRL < 50):
-                UpRL_V.config(text = "Value too low")
-            elif (UpRL > 175):
-                UpRL_V.config(text = "Value too high")
-            else:
-                UpRL_V.config(text = UpRL)
-                #write to file
-                UpRL_F = open("UpRL.txt", "w")
-                UpRL_F.write(UpRL_E.get())
-                UpRL_F.close()
-        except:
-            UpRL_V.config(text = "Invalid Value")
         
     def changeVentAmp():
         try:
@@ -143,10 +284,8 @@ def openVOOR():
                 VentAmp_V.config(text = "Value too high")
             else:
                 VentAmp_V.config(text = VentAmp)
-                #write to file
-                VentAmp_F = open("VentAmp.txt", "w")
-                VentAmp_F.write(VentAmp_E.get())
-                VentAmp_F.close()
+                VentAmp = VentAmp*10
+                set_params()
         except:
             VentAmp_V.config(text = "Invalid Value")
             
@@ -160,87 +299,69 @@ def openVOOR():
                 VentPW_V.config(text = "Value too high")
             else:
                 VentPW_V.config(text = VentPW)
-                #write to file
-                VentPW_F = open("VentPW.txt", "w")
-                VentPW_F.write(VentPW_E.get())
-                VentPW_F.close()
+                set_params()
         except:
             VentPW_V.config(text = "Invalid Value")
     
     def changemaxSensorRate():
         try:
             #check variable range
-            maxSensorRate = int(maxSensorRate_E.get())
-            if (maxSensorRate < 50):
+            MaxSR = int(maxSensorRate_E.get())
+            if (MaxSR < 50):
                 maxSensorRate_V.config(text = "Value too low")
-            elif (maxSensorRate> 175):
+            elif (MaxSR > 175):
                 maxSensorRate_V.config(text = "Value too high")
             else:
-                maxSensorRate_V.config(text = maxSensorRate)
-                #write to file
-                maxSensorRate_F = open("maxSensorRate.txt", "w")
-                maxSensorRate_F.write(maxSensorRate_E.get())
-                maxSensorRate_F.close()
+                maxSensorRate_V.config(text = MaxSR)
+                set_params()
         except:
             maxSensorRate_V.config(text = "Invalid Value")
     
     def changereactTime():
         try:
             #check variable range
-            reactTime = int(reactTime_E.get())
-            if (reactTime < 10):
+            ReacTime = int(reactTime_E.get())
+            if (ReacTime < 10):
                 reactTime_V.config(text = "Value too low")
-            elif (reactTime > 50):
+            elif (ReacTime > 50):
                 reactTime_V.config(text = "Value too high")
             else:
-                reactTime_V.config(text = reactTime)
-                #write to file
-                reactTime_F = open("reactTime.txt", "w")
-                reactTime_F.write(reactTime_E.get())
-                reactTime_F.close()
+                reactTime_V.config(text = ReacTime)
+                set_params()
         except:
             reactTime_V.config(text = "Invalid Value")
 
     def changerespFactor():
         try:
             #check variable range
-            respFactor = int(respFactor_E.get())
-            if (respFactor < 1):
+            RespF = int(respFactor_E.get())
+            if (RespF < 1):
                 respFactor_V.config(text = "Value too low")
-            elif (respFactor > 16):
+            elif (RespF > 16):
                 respFactor_V.config(text = "Value too high")
             else:
-                respFactor_V.config(text = respFactor)
-                #write to file
-                respFactor_F = open("respFactor.txt", "w")
-                respFactor_F.write(respFactor_E.get())
-                respFactor_F.close()
+                respFactor_V.config(text = RespF)
+                set_params()
         except:
             respFactor_V.config(text = "Invalid Value")
     
     def changerecoveryTime():
         try:
             #check variable range
-            recoveryTime = int(recoveryTime_E.get())
-            if (recoveryTime < 2):
+            RecTime = int(recoveryTime_E.get())
+            if (RecTime < 2):
                 recoveryTime_V.config(text = "Value too low")
-            elif (recoveryTime > 16):
+            elif (RecTime > 16):
                 recoveryTime_V.config(text = "Value too high")
             else:
-                recoveryTime_V.config(text = recoveryTime)
-                #write to file
-                recoveryTime_F = open("recoveryTime.txt", "w")
-                recoveryTime_F.write(recoveryTime_E.get())
-                recoveryTime_F.close()
+                recoveryTime_V.config(text = RecTime)
+                set_params()
         except:
             recoveryTime_V.config(text = "Invalid Value")
 
     # Create Entries
     LowRL_E = Entry(VOORPage, width=20)
     LowRL_E.insert(0, "Enter New Value")
-
-    UpRL_E = Entry(VOORPage, width=20)
-    UpRL_E.insert(0, "Enter New Value")
 
     VentAmp_E = Entry(VOORPage, width=20)
     VentAmp_E.insert(0, "Enter New Value")
@@ -262,7 +383,6 @@ def openVOOR():
 
     # Create Buttons
     LowRL_B = Button(VOORPage, text="Update", command=changeLowRL)
-    UpRL_B = Button(VOORPage, text="Update", command=changeUpRL)
     VentAmp_B = Button(VOORPage, text="Update", command=changeVentAmp)
     VentPW_B = Button(VOORPage, text="Update", command=changeVentPW)
     maxSensorRate_B= Button(VOORPage, text="Update", command=changemaxSensorRate)
@@ -279,40 +399,35 @@ def openVOOR():
     LowRL_E.grid(row= 2, column= 2)
     LowRL_B.grid(row= 2, column= 3)
 
-    UpRL_L.grid(row= 3, column= 0)
-    UpRL_V.grid(row= 3, column= 1)
-    UpRL_E.grid(row= 3, column= 2)
-    UpRL_B.grid(row= 3, column= 3)
+    VentAmp_L.grid(row= 3, column= 0)
+    VentAmp_V.grid(row= 3, column= 1)
+    VentAmp_E.grid(row= 3, column= 2)
+    VentAmp_B.grid(row= 3, column= 3)
 
-    VentAmp_L.grid(row= 4, column= 0)
-    VentAmp_V.grid(row= 4, column= 1)
-    VentAmp_E.grid(row= 4, column= 2)
-    VentAmp_B.grid(row= 4, column= 3)
+    VentPW_L.grid(row= 4, column= 0)
+    VentPW_V.grid(row= 4, column= 1)
+    VentPW_E.grid(row= 4, column= 2)
+    VentPW_B.grid(row= 4, column= 3)
 
-    VentPW_L.grid(row= 5, column= 0)
-    VentPW_V.grid(row= 5, column= 1)
-    VentPW_E.grid(row= 5, column= 2)
-    VentPW_B.grid(row= 5, column= 3)
+    maxSensorRate_L.grid(row= 5, column= 0)
+    maxSensorRate_V.grid(row= 5, column= 1)
+    maxSensorRate_E.grid(row= 5, column= 2)
+    maxSensorRate_B.grid(row= 5, column= 3)
 
-    maxSensorRate_L.grid(row= 6, column= 0)
-    maxSensorRate_V.grid(row= 6, column= 1)
-    maxSensorRate_E.grid(row= 6, column= 2)
-    maxSensorRate_B.grid(row= 6, column= 3)
+    reactTime_L.grid(row= 6, column= 0)
+    reactTime_V.grid(row= 6, column= 1)
+    reactTime_E.grid(row= 6, column= 2)
+    reactTime_B.grid(row= 6, column= 3)
+    
+    respFactor_L.grid(row= 7, column= 0)
+    respFactor_V.grid(row= 7, column= 1)
+    respFactor_E.grid(row= 7, column= 2)
+    respFactor_B.grid(row= 7, column= 3)
 
-    reactTime_L.grid(row= 8, column= 0)
-    reactTime_V.grid(row= 8, column= 1)
-    reactTime_E.grid(row= 8, column= 2)
-    reactTime_B.grid(row= 8, column= 3)
-
-    respFactor_L.grid(row= 9, column= 0)
-    respFactor_V.grid(row= 9, column= 1)
-    respFactor_E.grid(row= 9, column= 2)
-    respFactor_B.grid(row= 9, column= 3)
-
-    recoveryTime_L.grid(row= 10, column= 0)
-    recoveryTime_V.grid(row= 10, column= 1)
-    recoveryTime_E.grid(row= 10, column= 2)
-    recoveryTime_B.grid(row= 10, column= 3)
+    recoveryTime_L.grid(row= 8, column= 0)
+    recoveryTime_V.grid(row= 8, column= 1)
+    recoveryTime_E.grid(row= 8, column= 2)
+    recoveryTime_B.grid(row= 8, column= 3)
 
     #Statues Bar
  
