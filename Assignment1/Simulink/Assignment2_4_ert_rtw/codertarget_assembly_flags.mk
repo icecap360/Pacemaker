ee968e942bfa1e4ef85c7655e9ab1819f7738123
muhammad ali <muhammadaliog3@live.com>
2020-12-18 23:57:23 -0500

    Renaming files

--- Assignment1/Simulink/Assignment2_4_ert_rtw/codertarget_assembly_flags.mk
+++
@@ -1,7 +0,0 @@
-ASFLAGS_ADDITIONAL = -mcpu=cortex-m4 -mthumb -mfpu=fpv4-sp-d16 -mfloat-abi=hard -fexceptions -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -std=gnu99
-KinetisSDKRootDir = C:/FREESC~1/KSDK_1~1.0
-JLinkRootDir = C:/PROGRA~2/SEGGER/JLINK_~1
-TARGET_LOAD_CMD = 
-TARGET_LOAD_CMD_ARGS = 
-TARGET_PKG_INSTALLDIR = C:/PROGRA~3/MATLAB/SUPPOR~1/R2020a/toolbox/target/SUPPOR~1/FREEDO~1
-STACK_SIZE = 64
